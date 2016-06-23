7621acfa17d192e87c72f50c05eb11b669a683db
Miloš Milošević <milos.remix@gmail.com>
2016-06-23 10:22:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ any type of application. It supports both Android and iOS platforms.
 
 setup(
     name='pushkin',
-    version='0.1.3',
+    version='0.1.4',
 
     description='Pushkin is a free open source tool for sending push notifications',
     long_description=long_description,
